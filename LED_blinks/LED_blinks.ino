73a6cb0c829337557e0057f8ee090509f3d15553
Russell Fincham <rfincham@hotmail.co.uk>
2015-08-10 19:31:28 +0100

    Initial code

---
+++ LED_blinks/LED_blinks.ino
@@ -0,0 +1,21 @@
+//Uses LED L on the Arduino Uno
+
+int ledPin =13: //LED is connected to LED pin 13
+
+//The seup routine configures the digital port
+//This routine is executed only once at program start!
+
+void setup()
+{
+  // The pin is configured as output
+  pinMode(ledPin, OUTPUT);
+}
+
+  // The main program is an infinite loop
+void loop()
+{
+  digitalWrite(ledPin, HIGH); //Turn on LED
+  delay(1000);                //Wait for a second
+  digitalWrite(ledPin, LOW);  //Turn off LED
+  delay(1000);                //Wait for a second
+}
