d4133d659eecd6ad50744625b4b4ddea736e50bf
Russell Fincham <rfincham@hotmail.co.uk>
2015-08-13 14:49:28 +0100

    Else_If_and_SwitchCase

---
+++ else_if/else_if.ino
@@ -0,0 +1,32 @@
+// Else ... If
+
+int x;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Else ... If statements");
+  Serial.println();
+}
+
+void loop()
+{
+
+  if(x==1)
+  {
+    Serial.println("Text 1");
+  }
+  else if (x==10)
+  {
+    Serial.println("Text 10");
+    while(1);
+  }
+  else
+  {
+    Serial.print("X = ");
+    Serial.print(x);
+    Serial.println();
+  }
+  x++;
+}
+
